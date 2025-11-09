e9773fd3db401036ee8ecdd77e33489e2fb67c34
Felipe Tagawa <felipereisbkp@gmail.com>
2025-11-09 14:11:23 -0300

    fix: final fix

--- AtividadeMaven/src/main/java/org/example/Main.java
+++ AtividadeMaven/src/main/java/org/example/Main.java
@@ -1,14 +1,8 @@
 package org.example;
 
-import org.apache.commons.io.FileUtils;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String conteudo = FileUtils.readFileToString(new File("mensagem.txt"), StandardCharsets.UTF_8);
-        System.out.println(conteudo);
-        FileUtils.writeStringToFile(new File("mensagem.txt"), "Oi Zeca", StandardCharsets.UTF_8);
+    public static void main(String[] args) {
+        Addpdf addNovoPDF = new Addpdf();
+        addNovoPDF.addpdf();
     }
 }
